00a28eba6997c266f6397ecdd2beec05404ab386
Karina Quiroga <iquiroga5@estudiantes.areandina.edu.co>
2024-11-02 16:37:37 -0500

    Comentario

--- Prueba.py
+++ Prueba.py
@@ -1,4 +1,5 @@
 print("Hola Mundo 2")
 Edad = 21
 FechaNacimiento = "29/09/2003"
+# Imprimiendo un algo
 print(f"Hola mi nombre es Karina , Tengo {Edad} años y mi fecha de nacimiento fue {FechaNacimiento}")
